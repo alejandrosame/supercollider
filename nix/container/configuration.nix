b203a2f7b7cbddca8a07c0a963db5083b5c1d28c
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-08-16 10:54:48 +0200

    sadness

--- nix/container/configuration.nix
+++ nix/container/configuration.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
-{ 
+{
+  imports = [ ./modules/binbash.nix ];
+
   boot.isContainer = true;
 
   services.openssh.enable = true;
@@ -20,13 +22,25 @@
     ];
   };
 
+  security.sudo.wheelNeedsPassword = false;
+
   networking.hostName = lib.mkDefault "test";
   networking.useDHCP = false;
   
   services.avahi.enable = true;
 
+  # Research how to add envfs in an older NixOS
+  # Needed to be able to bind wayland socket like
+  # Meanwhile import module from https://discourse.nixos.org/t/add-bin-bash-to-avoid-unnecessary-pain/5673/10
+  environment.binbash = pkgs.bash;
+  # Problems with
+  # `sudo systemd-nspawn --bind=/run/user/1000/wayland-0:/run/user/1000/wayland-0 -D /var/lib/nixos-containers/test --boot`
+  # Relevant issue: https://github.com/NixOS/nixpkgs/issues/28833
+  environment.etc."os-release".mode = "0644";
+
   environment.systemPackages =
     (with pkgs; [
+      git
       nano
       libudev
     ]);
@@ -35,4 +49,17 @@
     UDEVPATH="${pkgs.libudev}";
     UDEVPATHDEV="${pkgs.libudev.dev}";
   };
+
+  # /usr/lib/systemd/systemd, /lib/systemd/systemd, /sbin/init
+
+  # Ref: https://github.com/NixOS/nixpkgs/issues/9884#issue-106733381
+  system.activationScripts.installInitScript = lib.mkForce ''
+    mkdir -p /sbin
+    mkdir -p /usr/lib
+    mkdir -p /lib
+
+    ln --force --symbolic --relative /nix/var/nix/profiles/system/init /sbin/init
+    ln --force --symbolic --relative /nix/var/nix/profiles/system/systemd/lib/systemd /usr/lib/systemd
+    ln --force --symbolic --relative /nix/var/nix/profiles/system/systemd/lib/systemd /lib/systemd
+  '';
 }
